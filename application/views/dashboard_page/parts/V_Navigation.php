3857744073e42a410edd18fb4912262e3e84755e
Utut Ardiansah <ututardia@gmail.com>
2019-08-01 11:23:22 +0700

    Dashboard Page Update

--- application/views/dashboard_page/parts/V_Navigation.php
+++ application/views/dashboard_page/parts/V_Navigation.php
@@ -10,7 +10,7 @@ $name = $this->session->userdata('nama_user');
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="navbar-brand-wrapper d-flex justify-content-center">
             <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
-                <a class="navbar-brand brand-logo" href="<?php echo base_url()?>assets/dashboard_page/index.html"><img src="<?php echo base_url()?>assets/dashboard_page/images/logo.svg" alt="logo"/></a>
+                <a class="navbar-brand brand-logo" href="<?php echo base_url()?>assets/dashboard_page/index.html"><img src="<?php echo base_url()?>assets/dashboard_page/images/Homelogo1.png" alt="logo"/></a>
                 <a class="navbar-brand brand-logo-mini" href="<?php echo base_url()?>assets/dashboard_page/index.html"><img src="<?php echo base_url()?>assets/dashboard_page/images/logo-mini.svg" alt="logo"/></a>
                 <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
                     <span class="mdi mdi-sort-variant"></span>
